941c3b5a3a353b528f3437f5b574bd6253e3b693
Art-Wolf <john.c.doyle@gmail.com>
2012-12-20 02:07:55 -0500

    db update

--- database/mafia_game_structures.sql
+++ database/mafia_game_structures.sql
@@ -134,7 +134,7 @@ CREATE TABLE `logins` (
   `login_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -245,4 +245,4 @@ CREATE TABLE `votes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-12-20  1:29:55
+-- Dump completed on 2012-12-20  2:07:42
